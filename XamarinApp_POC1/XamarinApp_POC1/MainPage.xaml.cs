8d74e0e774f6eeb958974f281fcd67e7b79fc676
IvanPostu <ipostu20000127@gmail.com>
2022-02-06 18:22:30 +0200

    Init project

---
+++ XamarinApp_POC1/XamarinApp_POC1/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinApp_POC1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Image1.Source = ImageSource.FromResource("XamarinApp_POC1.Assets.Images.img1.png");
+
+            Image1.Aspect = Aspect.AspectFill;
+        }
+    }
+}
